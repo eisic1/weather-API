1606541ecf1fb84abc268b09614923f604c8114a
eisic1 <102618760+eisic1@users.noreply.github.com>
2023-08-31 18:58:48 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     openWeatherMap: {
         current_url: "https://api.openweathermap.org/data/2.5/weather?q=",
-        secret_key: "a1afd38e18626792ceeb3ab2dc5ad6ed",
+        secret_key: "",
         forecast_url: "https://api.openweathermap.org/data/2.5/forecast"
     }
 }
